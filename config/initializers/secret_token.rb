733ebe55639e5f6e14822d867a5e26266c30e7ba
Dave Woodall <dmwoodall@gmail.com>
2012-05-28 12:41:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperwhiteApp::Application.config.secret_token = '32340466f4ff16f721f5671fa3156524289192dc6566c8665e0704fc41e5f13e82cb4532dba1dd244f7a80699059a8156b573aca963f28b57bfd86b91fbf1eaf'
