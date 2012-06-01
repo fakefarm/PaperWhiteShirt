b860ae8c3e2d358cf5e5fd6cb9a152704c60d226
Dave Woodall <dmwoodall@gmail.com>
2012-06-01 18:27:17 -0500

    contact page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,24 @@
 class PagesController < ApplicationController
+	
 	def landing
 	end
 	
 	def about
+	end
+	
+	def contact
+	end
+	
+	def stores
+	end
+	
+	def press
+	end
+	
+	def spring_2012
+	end
+	
+	def fall_2012
 	end
 	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ PaperwhiteApp::Application.routes.draw do
 	root to: "pages#landing"
 
 	match "/about" => "pages#about"
+	match "/contact" => "pages#contact"
+	match "/press" => "pages#press"
+	match "/stores" => "pages#stores"
+	match "/fall-2012" => "pages#fall_2012"
+	match "/spring-2012" => "pages#spring_2012"
+	
 end
