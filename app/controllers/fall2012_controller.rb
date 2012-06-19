d805772e82fa3039c8f22d31a8c1266d17d48904
Dave Woodall <dmwoodall@gmail.com>
2012-06-19 12:49:48 -0500

    social links and updated fall product styling

--- app/controllers/fall2012_controller.rb
+++ app/controllers/fall2012_controller.rb
@@ -1,4 +1,41 @@
 class Fall2012Controller < ApplicationController
 	def collection
 	end
+	
+	def drape_front_jacket
+	end
+
+	def faux_leather_blazer
+	end
+
+	def printed_jacket
+	end
+		
+	def tuxedo_blazer
+	end
+		
+	def fitted_denim_jacket
+	end
+		
+	def faux_leather_motorcycle_jacket
+	end
+
+	def sequined_top_riding_pant
+	end
+		
+	def embroidered_plaid_shirt
+	end
+	
+	def stripe_blouse_wide_leg_pant
+	end
+		
+	def printed_knit_top_pleated_skirt
+	end
+		
+	def novelty_blouse_printed_pant
+	end
+		
+	def novelty_pleat_shirt
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,26 @@ PaperwhiteApp::Application.routes.draw do
 	match "/contact" => "pages#contact"
 	match "/press" => "pages#press"
 	match "/stores" => "pages#stores"
-	match "/fall-2012" => "fall2012#collection"
-	match "/spring-2012" => "pages#spring_2012"
 	match	"/designer" => "pages#designer"
 	
+	
+	match "/fall-2012" => "fall2012#collection"
+	match "/drape-front-jacket" =>  "fall2012#drape_front_jacket"
+	match "/faux-leather-blazer" => "fall2012#faux_leather_blazer"
+	match "printed-jacket" =>	"fall2012#printed_jacket"
+	match "tuxedo-blazer" =>	"fall2012#tuxedo_blazer"
+	match "fitted-denim-jacket" =>	"fall2012#fitted_denim_jacket"
+	match "faux-leather-motorcycle-jacket" =>	"fall2012#faux_leather_motorcycle_jacket"
+	match "sequined-top-riding-pant" =>	"fall2012#sequined_top_riding_pant"
+	match "embroidered-plaid-shirt" =>	"fall2012#embroidered_plaid_shirt"
+	match "stripe-blouse-wide-leg-pant" =>	"fall2012#stripe_blouse_wide_leg_pant"
+	match "printed-knit-top-pleated-skirt" =>	"fall2012#printed_knit_top_pleated_skirt"
+	match "novelty-blouse-printed-pant" =>	"fall2012#novelty_blouse_printed_pant"
+	match "novelty-pleat-shirt" =>	"fall2012#novelty_pleat_shirt"
+		
 end
+	
+	
+	
+		
+
